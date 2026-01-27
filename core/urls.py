ab90a6bfd8d14d1230b3fb17956a83050b33542a
official-noman <official.aanoman@gmail.com>
2026-01-28 03:38:36 +0600

    dashboard redirect to admin panel

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')), 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('', RedirectView.as_view(url='/admin/', permanent=False), name='home'),
 ]
 
 # Serving media files during development (Required for Excel processing)
